38e2aae08bd40b713c84236607e63d83970a493a
Ксения Лучкова <gorodskayafialka@gmail.com>
2020-11-29 01:57:19 +0300

    Small edits

--- src/lab2/Main.java
+++ src/lab2/Main.java
@@ -5,42 +5,29 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
+        Shape maxPerimeter, maxArea , minPerimeter, minArea;
         ArrayList<Shape> shapes = new ArrayList<>();
-        try {
             shapes.add(new Triangle(30,40,50));
+        try {
+            shapes.add(new Circle(-1200000));
         }
         catch (IllegalArgumentException e) {
             System.out.println(e);
         }
-        try {
-            shapes.add(new Circle(-20));
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-        try {
             shapes.add(new Rectangle(4, 7));
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-        try {
             shapes.add(new Square(10));
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println(e);
-        }
-
             shapes.add(new Square(33.5));
-            try{
-                shapes.add(new Circle(-120000));
-            }
-            catch (IllegalArgumentException e)
-            {
-                System.out.println(e);
-            }
             double sumArea = 0;
-            Shape maxPerimeter = shapes.get(0), maxArea = shapes.get(0), minPerimeter = shapes.get(0), minArea = shapes.get(0);
+            try{
+            maxPerimeter = shapes.get(0);
+            maxArea = shapes.get(0);
+            minPerimeter = shapes.get(0);
+            minArea = shapes.get(0);}
+            catch (ArrayIndexOutOfBoundsException e)
+            {
+                System.out.println("Empty list");
+                return;
+            }
             for (Shape current : shapes) {
                 if (current.calcArea() > maxArea.calcArea())
                     maxArea = current;
